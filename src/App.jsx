e6b320a76bcb00fea60bf81cd17bdbb8385549cd
borismg04 <borismg04@gmail.com>
2022-07-12 11:41:25 -0500

    conceptos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hola mundo</h1>
+        <p>Un parrafo</p>
+      </div>
+    </>
   )
 }
 
