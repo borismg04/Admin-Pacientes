8af4b51e443f436b0e6857716d651f598056fc1b
borismg04 <borismg04@gmail.com>
2022-07-19 17:15:47 -0500

    hooks

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,30 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre:{" "} 
+          <span className="font-normal normal-case">Nombre Paciente</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario:{" "} 
+          <span className="font-normal normal-case">Boris</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email:{" "} 
+          <span className="font-normal normal-case">correo@correo</span>
+        </p>
+        
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta:{" "} 
+          <span className="font-normal normal-case">10/10/2022</span>
+        </p>
+
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas:{" "} 
+          <span className="font-normal normal-case">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+          </span>
+        </p>
+      </div>
+  )
+}
+
+export default Paciente
