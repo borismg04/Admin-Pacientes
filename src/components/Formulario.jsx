b2dfe9e3c75c46859fda11b34df74bb56bba98a5
borismg04 <borismg04@gmail.com>
2022-07-19 19:41:05 -0500

    hooks

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,9 @@ import { useState,useEffect } from "react"
 
 
 const Formulario = () => {
+
+  const [nombre,setNombre]=useState("")
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
