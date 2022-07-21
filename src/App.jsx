8d96144177e6f1dd24ee89d8ca4ca025bbc7c8ea
borismg04 <borismg04@gmail.com>
2022-07-21 16:40:58 -0500

    props

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Formulario from "./components/Formulario"
 import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
+
+  const [pacientes, setPacientes] = useState([])
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
