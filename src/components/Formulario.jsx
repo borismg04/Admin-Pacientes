e0b43d40c956fce4067517fa39d9d330cde9f473
borismg04 <borismg04@gmail.com>
2022-07-25 12:07:57 -0500

    props error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,5 @@
 import { useState,useEffect } from "react"
+import Error from "./Error";
 
 
 const Formulario = ({pacientes,setPacientes}) => {
@@ -39,7 +40,7 @@ const Formulario = ({pacientes,setPacientes}) => {
     setEmail("");
     setFecha("");
     setSintomas("");
-    
+
   }
 
 
@@ -56,11 +57,9 @@ const Formulario = ({pacientes,setPacientes}) => {
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
         onSubmit={handleSubmit}
       >
-        {error && 
-          (<div>
-            <p className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">Todos los campos son obligatorios</p>
-          </div>
-          )}
+        {error &&  <Error 
+          mensaje="Todos los campos son obligatorios"
+        />}
 
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
-const error = () => {
+const error = ({mensaje}) => {
   return (
-    <div>error</div>
-  )
-}
+    <div className="bg-red-600 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+      <p>{mensaje}</p>
+    </div>
+  );
+};
 
-export default error
+export default error;
