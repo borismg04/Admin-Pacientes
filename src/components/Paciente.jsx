586695780b97d1b176d5d8176f9c4df89c306064
borismg04 <borismg04@gmail.com>
2022-07-26 14:17:43 -0500

    LocalStroge

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,13 @@
-const Paciente = ({paciente,setPaciente}) => {
+const Paciente = ({paciente,setPaciente,eliminarPaciente}) => {
 
-  const {nombre, propietario, email, fecha, sintomas} = paciente;
+  const {nombre, propietario, email, fecha, sintomas,id} = paciente;
+
+  const handleEliminar = () => {
+    const respuesta = window.confirm("¿Estas seguro de eliminar este paciente?");
+    if(respuesta){
+      eliminarPaciente(id);
+    }
+  }
 
   return (
     <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
@@ -36,6 +43,8 @@ const Paciente = ({paciente,setPaciente}) => {
           <button 
             type="button"
             className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+            onClick={handleEliminar}
+
             >Eliminar</button>
 
 
